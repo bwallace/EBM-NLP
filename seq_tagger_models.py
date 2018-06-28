469e8aa4bdedb3a0bfce26b998f2ce087bde033d
bwallace <byron.wallace@gmail.com>
2018-06-28 15:47:39 -0400

    small additions

--- seq_tagger_models.py
+++ seq_tagger_models.py
@@ -25,7 +25,7 @@ class LSTMTagger(nn.Module):
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, bidirectional=True)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2, bidirectional=True)
 
         # The linear layer that maps from hidden state space to tag space
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
